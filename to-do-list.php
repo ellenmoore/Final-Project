69d035bf9172d9f70929abab0c16c90157134656
Ellen Moore <ellenmoore13@gmail.com>
2016-11-18 18:56:09 -0600

    Add files via upload
    Initial HTML & CSS files for Final Project

---
+++ to-do-list.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>My To Do List</title>
+<link rel="stylesheet" href="to-do-styles.css" />
+</head>
+<body>
+<header>
+	<div id="myDIV" class="header">
+		<h2>My To Do List</h2>
+		<input type="text" id="myInput" placeholder="Title...">
+		<span class="addBtn">Add</span>
+	</div>
+</header>
+	<ul id="myUL">
+		<li>first to do</li>
+		<li>second to do</li>
+		<li>third to do</li>
+	</ul>
+
+</body>
+</html>
