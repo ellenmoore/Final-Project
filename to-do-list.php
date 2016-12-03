c468a5349fa0a0db652b3bb0a30625686d3b1c72
Ellen Moore <ellenmoore13@gmail.com>
2016-12-03 12:39:12 -0600

    Update to-do-list.php
    add php to use $_POST to get new task and display it in list

--- to-do-list.php
+++ to-do-list.php
@@ -1,3 +1,13 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if (isset($_POST['task'])){
+	$task = $_POST['task'];
+}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,19 +16,20 @@
 <script type="text/JavaScript" src="to-do-java.js"></script>
 </head>
 <body>
-
+	<form method="POST">
 	<div id="myDIV" class="header">
 		<h2>My To Do List</h2>
-		<input type="text" id="myInput" placeholder="Title...">
-		<span class="addBtn">Add</span>
+		<input type="text" id="myInput" name="task" placeholder="Add new task">
+		<input type="submit" class="addBtn" value="Add">
 	</div>
-
-	<ul id="myUL">
+	</form>
 	
-		<li>first to do</li>
+	<ul id="myUL">
+		
+		<li> <?php echo $task; ?> </li>
 		<li>second to do</li>
 		<li>third to do</li>
 	</ul>
-
+	
 </body>
 </html>
