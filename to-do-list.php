298ff190cbf95b5a632aa3bf9c480b88f67108ca
Ellen Moore <ellenmoore13@gmail.com>
2016-12-14 18:21:21 -0600

    Update to-do-list.php
    add php and mysql

--- to-do-list.php
+++ to-do-list.php
@@ -1,6 +1,27 @@
 <?php
+require_once('config.php');
+$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
-$task = $_POST['task'];
+
+//handling connection errors
+if (mysqli_connect_errno()) {
+	die(mysqli_connect_error());
+}
+
+//define variable to hold new task entered
+$task = $_POST['newTask'];
+//select all tasks from table
+$taskSql = "SELECT task, id FROM tasks ORDER BY id asc";
+$taskResult = mysqli_query($connection, $taskSql);
+//add new task to table
+$insertSql = "INSERT INTO tasks (task) VALUES ('$task')";
+$insertResult = mysqli_query($connection, $insertSql);
+//define variable to hold $taskId retrieved from delete form. This is then used in the delete query.
+$taskIdDel=$_POST['taskId'];
+//delete task from table
+$deleteSql="DELETE FROM tasks WHERE id='$taskIdDel'";
+$deleteResult=mysqli_query($connection, $deleteSql);
+
 
 ?>
 
@@ -15,14 +36,32 @@ $task = $_POST['task'];
 	<form method="POST">
 	<div id="myDIV" class="header">
 		<h2>My To Do List</h2>
-		<input type="text" id="myInput" name="task" placeholder="Add new task">
+		<input type="text" id="myInput" name="newTask" placeholder="Add new task" required>
 		<input type="submit" class="addBtn" value="Add">
 	</div>
 	</form>
 	
 	<ul id="myUL">
 		
-		<li> <?php echo $task; ?> </li>
+		<?php 
+		//display the tasks from the database
+		if ($taskResult = mysqli_query($connection, $taskSql)) {
+			while($row = mysqli_fetch_assoc($taskResult)){
+					
+					echo '<form method="POST">';
+					//define variables to hold task and id # from database
+					$taskName = $row['task'];
+					$taskId = $row['id'];
+					echo '<li><span>'.$taskName.'</span>';
+					//create a button that will delete a task from the database. 
+					//Use the $taskId to know which task to delete.
+					echo '<input type="submit" id="'.$taskId.'" class="close" value="X">';
+					echo '</li></form>';
+			}
+		}
+		
+		?>
+		
 		
 	</ul>
 	
