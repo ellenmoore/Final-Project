4fee12911c2d17d7e2a775d44e1d9f20dfa571ab
Ellen Moore <ellenmoore13@gmail.com>
2016-12-15 19:07:28 -0600

    Update todo.sql

--- todo.sql
+++ todo.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Aug 20, 2013 at 03:46 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `bookcrm`
---
-
--- --------------------------------------------------------
-
-
-
 --
 -- Table structure for table `taskss`
 --
@@ -49,8 +23,3 @@ INSERT INTO `tasks` (`ID`, `Task`) VALUES
 
 -- --------------------------------------------------------
 
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
